240ba3a1b676499565794195e526265da1afc86a
Oladeji <Akomspatrick@yahoo.com>
2021-04-08 00:28:24 +0100

    Initial

---
+++ NewHonlecturerProject/GradeManagerapp/admin.py
@@ -0,0 +1,6 @@
+from django.contrib import admin
+
+# Register your models here.
+admin.site.site_header ="MR LECTURER"
+admin.site.site_title ="PMR LECTURER ADMIN AREA" 
+admin.site.index_title ="WELCOME TO MR LECTURER ADMIN"
